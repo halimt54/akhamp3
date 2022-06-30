e4f6bf3edf4697423590d9ab7ee4ea697592058d
Akif Yener <yener@terraeds.com>
2022-06-30 10:55:02 -0400

    get playlist song param updated

--- src/index.js
+++ src/index.js
@@ -359,8 +359,8 @@ streamerApi.delete('/playlistSong', async (req, res) => {
 // get playlist songs
 streamerApi.get('/playlistSong/:id', async (req, res) => {
     try {
-        const id = req.params.id
-        if (!id) {
+        const playlistId = req.params.id
+        if (!playlistId) {
             return res.status(400).send({
                 message: "playlist id is missing"
             })
